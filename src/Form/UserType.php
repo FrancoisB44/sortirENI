1de13b18e53acca89d233e3c263f6ef1d794340f
FrancoisB44 <fbr.brunet@gmail.com>
2021-04-08 09:49:10 +0200

    //creation : -AdminController -Form:xxxType -page admin ajout Campus et Status

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo')
+            ->add('nameUser')
+            ->add('firstNameUser')
+            ->add('phoneNumber')
+            ->add('mail')
+            ->add('password')
+            ->add('roles')
+            ->add('admin')
+            ->add('active')
+            ->add('inscription')
+            ->add('campus')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
