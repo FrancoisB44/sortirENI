4f9bd7c68e7a69bbd2810088c5f641e302c7ea60
agnes-guillet21 <agnes.guillet@hotmail.fr>
2021-04-12 12:12:45 +0200

    test requete ajax

---
+++ src/Controller/AjaxController.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Repository\CityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AjaxController extends AbstractController
+{
+
+    /**
+     * @Route("/demo",name="demo")
+     */
+
+    public function index(): Response
+    {
+        return $this->render('event/ajax.html.twig',[]);
+    }
+
+    //creation de la route qui va afficher une liste de ville
+    /**
+     * @Route ("/demo/villes/recherche", name="demo_city_search")
+     */
+    public function citySearch(CityRepository $cityRepository, Request $request): Response
+    {
+        $keyword=$request->query->get('keyword'); //le nom du parametre d url
+       $results=  $cityRepository->searchCity($keyword); // methode inventee je passe mes mot cles en argumentss
+        return $this->render("event/ajax_cities.html.twig",[
+            "cities"=>$results
+        ]);// renvoyer un morceaud  html avc les resultats dedans
+    }
+
+
+}
