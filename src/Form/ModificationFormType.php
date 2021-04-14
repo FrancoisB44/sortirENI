c5929f523f97e6d6b69c8d7918df229f43737ac5
agnes-guillet21 <agnes.guillet@hotmail.fr>
2021-04-14 17:34:29 +0200

    front-end page

--- src/Form/ModificationFormType.php
+++ src/Form/ModificationFormType.php
@@ -37,23 +37,23 @@ class ModificationFormType extends AbstractType
                 'multiple' => false,
         ])
 
-            ->add('plainPassword', RepeatedType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-                'type' => PasswordType::class,
-                'invalid_message' => 'The password fields must match.',
-                'options' => ['attr' => ['class' => 'password-field']],
-                'required' => false,
-                'first_options'  => ['label' => 'Mot de passe : '],
-                'second_options' => ['label' => 'Répétez le mot de passe : '],
-            ])
+//            ->add('plainPassword', RepeatedType::class, [
+//                'mapped' => false,
+//                'constraints' => [
+//                    new Length([
+//                        'min' => 6,
+//                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+//                        // max length allowed by Symfony for security reasons
+//                        'max' => 4096,
+//                    ]),
+//                ],
+//                'type' => PasswordType::class,
+//                'invalid_message' => 'The password fields must match.',
+//                'options' => ['attr' => ['class' => 'password-field']],
+//                'required' => false,
+//                'first_options'  => ['label' => ' '],
+//                'second_options' => ['label' => ' '],
+//            ])
             ->add('picture', FileType::class, [
                 'label' => 'Image de profil',
                 'mapped' => false,//pas associe a une entite
